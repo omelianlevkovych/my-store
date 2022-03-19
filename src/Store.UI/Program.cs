17606566b76007a72f89d287454322db1d98093f
OmelianLevkovych <omelianai@gmail.com>
2022-03-19 23:34:06 +0200

    setup dbContext added connection string

--- src/Store.UI/Program.cs
+++ src/Store.UI/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Store.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
