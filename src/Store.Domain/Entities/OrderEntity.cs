9f4e417d99e5192b71624d555328e85b0fcf3de0
$sudo whoami <omelianai@gmail.com>
2022-03-20 01:34:32 +0200

    added entities: (#2)
    * stock
    * order
    * orderProduct
    added db migration

---
+++ src/Store.Domain/Entities/OrderEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.Domain.Entities
+{
+    public class OrderEntity
+    {
+        public int Id { get; set; }
+        public string Reference { get; set; }
+
+        // TODO: move the below props as separate object; single-responsibility.
+        public string MainAddress { get; set; }
+        public string SecondaryAddress { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public ICollection<ProductOrderEntity> ProductOrders { get; set; }
+    }
+}
---
+++ src/Store.Domain/Entities/ProductOrderEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Domain.Entities
+{
+    public class ProductOrderEntity
+    {
+        public int OrderId { get; set; }
+        public OrderEntity Order { get; set; }
+        public int ProductId { get; set; }
+        public ProductEntity Product { get; set; }
+    }
+}
